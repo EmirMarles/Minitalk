8d2b1f60e7dbf9c4786bbfd85d5ef4483f644105
Emir Marles <emarles@c4r1s6.42warsaw.pl>
2025-03-30 15:57:56 +0200

    few changes

--- Minitalk/client.c
+++ Minitalk/client.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   fdf_algo.c                                         :+:      :+:    :+:   */
+/*   client.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: emarles <emarles@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/02/22 12:02:33 by emarles           #+#    #+#             */
-/*   Updated: 2025/03/02 15:20:26 by emarles          ###   ########.fr       */
+/*   Created: 2025/03/29 17:27:26 by emarles           #+#    #+#             */
+/*   Updated: 2025/03/29 17:27:26 by emarles          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,44 +14,55 @@
 
 //SIGUSR1
 //SIGUSR2
-static void ft_send_signal(pid_t pid, unsigned char c);
+static void ft_send_signal(pid_t pid, char *string);
 static void ft_send_bit(pid_t pid, unsigned int bit);
+static void ft_positive_answer (int sig);
 
 int main(int argc, char*argv[])
 {
     pid_t pid;
-    int i;
-    char *string;
-    char c;
     if (argc < 3)
-        printf("not enough arguments\n");
-    else
     {
-        pid = ft_atoi(argv[1]);
-        string = ft_strdup(argv[2]);
-        printf("the string is %s\n", string);
-        i = 0;
-        while (string[i] != '\0') // to send the signal
-        {
-            c = string[i];
-            ft_send_signal(pid, c);
-            i++;
-            usleep(42);
-        }
-        free(string);
+        printf("not enough arguments\n");
+        return (0);
     }
+    pid = ft_atoi(argv[1]);
+    printf("the string is %s\n", argv[2]);
+    signal(SIGUSR1, ft_positive_answer);
+    ft_send_signal(pid, argv[2]);
     return (0);
 }
 
-static void ft_send_signal(pid_t pid, unsigned char c)
+static void ft_positive_answer (int sig)
+{
+    if (sig == SIGUSR1)
+        write(1, "The message has been succesfully sent :D\n", 40);
+}
+
+static void ft_send_signal(pid_t pid, char  *string)
 {
     int i;
+    int j;
+    char c;
 
-    i = 7;
-    while (i >= 0)
+    i = 0;
+    while (string[i] != '\0') // to send the signal
     {
-        ft_send_bit(pid, (c >> i) & 1);
-        i--;
+        c = string[i];
+        j = 7;
+        while (j >= 0)
+        {
+            ft_send_bit(pid, (c >> j) & 1);
+            j--;
+        }
+        i++;
+    }
+    c = '\0';
+    j = 7;
+    while (j >= 0)
+    {
+        ft_send_bit(pid, (c >> j) & 1);
+        j--;
     }
 }
 static void ft_send_bit(pid_t pid, unsigned int bit)
@@ -60,5 +71,5 @@ static void ft_send_bit(pid_t pid, unsigned int bit)
         kill(pid, SIGUSR1);
     else 
         kill(pid, SIGUSR2);
-    usleep(10000);
+    usleep(1);
 }
--- Minitalk/server.c
+++ Minitalk/server.c
@@ -1,58 +1,59 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   fdf_algo.c                                         :+:      :+:    :+:   */
+/*   server.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: emarles <emarles@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/02/22 12:02:33 by emarles           #+#    #+#             */
-/*   Updated: 2025/03/02 15:20:26 by emarles          ###   ########.fr       */
+/*   Created: 2025/03/29 17:27:49 by emarles           #+#    #+#             */
+/*   Updated: 2025/03/29 17:27:49 by emarles          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
-// server receives 
-volatile int received_bits = 0;
-volatile unsigned char received_char = 0;
 
-static void ft_receive_bit(int sig);
 
-int main(void)
+void ft_receive_bit(int sig, siginfo_t *info, void* context)
 {
-    struct  sigaction sa;
-    // if (argc < 1)
-    //     return 0;
-    sa.sa_handler = ft_receive_bit;
-    sigemptyset(&sa.sa_mask);
-    sa.sa_flags = 0;
-
-    sigaction (SIGUSR1, &sa , NULL);
-    sigaction (SIGUSR2, &sa , NULL);
-
-    printf("The process id: %i\n",getpid());
-    printf("Waiting for the message...\n");
-
-
-    while (1)
-        pause();
-    return (0);
-}
-
-// static void ft_signal_handler(int sig,  siginfo_t *info, void * context)
-// {
-//     printf("the signal %i is received\n", sig);
-// }
-
-static void ft_receive_bit(int sig)
-{
-    received_char = (received_char << 1) | (sig == SIGUSR2);
+    static unsigned char received_char = 0;
+    static unsigned int received_bits = 0;
+    int pid = 0;
+    (void)context;
+    if (!pid)
+        pid = info->si_pid;
+    received_char = (received_char << 1) | (sig == SIGUSR2);// sets the received bit to 1 if SIGUSR2 or to 0 if SIGUSR1
     received_bits++;
-
     if (received_bits == 8)
     {
-        printf("%c", received_char);
-        fflush(stdout);  // Ensure output is printed immediately
+        write(1, &received_char, 1);
+        if (received_char == '\0')
+        {
+
+            write(1, "\nThe message has been fully received\n", 38);
+            kill(pid, SIGUSR1);
+            pid = 0;
+        }
         received_bits = 0;
         received_char = 0;
     }
+}
+int main(void)
+{
+    struct  sigaction sa;
+    pid_t   pid;
+
+    sa.sa_flags = SA_SIGINFO;
+    sa.sa_sigaction = ft_receive_bit;
+    sigemptyset(&sa.sa_mask);
+
+    sigaction(SIGUSR1, &sa , NULL); // how a proccess should handle a new signal
+    sigaction(SIGUSR2, &sa , NULL);//(1st is the signal, 2nd is the pointer to a sig struct , 4rd where the old sig info should be saved)
+
+    pid = getpid();
+    printf("The process id: %i\n", pid);
+    printf("Waiting for the message...\n");
+
+    while (1)
+    pause();
+    return (0);
 }
